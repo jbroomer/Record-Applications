b08767da0c322fb8864f60774882f1248bb07225
Jack <jackc2849@gmail.com>
2019-12-26 17:37:58 -0500

    File for axios(HTTP Calls)

--- frontend/src/services/apps.js
+++ frontend/src/services/apps.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3001/database';
+const baseURL = 'http://localhost:3001/view';
 
 const getApp = () => {
     const request = axios.get(baseURL);

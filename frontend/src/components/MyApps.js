4c655babf22bcf9c0e81ae7948eeb239392de54f
Jack <jackc2849@gmail.com>
2019-12-26 17:35:05 -0500

    Remove duplicates

--- frontend/src/components/MyApps.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyApps = ({ databases }) => {
-    return (
-        databases.map(database => <li key={database.name}>{database.name} {database.age}</li>)
-    )
-}
-
-export default MyApps;

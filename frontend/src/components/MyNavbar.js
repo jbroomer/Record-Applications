1b778f48abf2caca435dc4856f7a03143b0d3eed
Jack <jackc2849@gmail.com>
2019-12-26 17:37:01 -0500

    Implemented the Navbar

---
+++ frontend/src/components/MyNavbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const MyNavbar = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="/">Record-Applications</Navbar.Brand>
+            <Navbar.Collapse>
+            <Nav className="mr-auto">
+            <Nav.Link href="/add">Add Apps</Nav.Link>
+            <Nav.Link href="/view">View Apps</Nav.Link>
+            </Nav>
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/account">Account</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                <NavDropdown.Item href="/">Logout</NavDropdown.Item>
+            </NavDropdown>
+        </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default MyNavbar;
